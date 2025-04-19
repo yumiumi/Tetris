b5ee35309add40c29468a9841abe89ce8b6f8302
Yumi <galkina1611@gmail.com>
2025-04-19 15:39:27 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "raylib.h"
+#include "raymath.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cassert>
+#include <intrin.h>
+using namespace std;
+
+int main() {
+	return 0;
+}
